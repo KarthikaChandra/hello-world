b6b6ca711713042df414740e0e31d57336da3c97
Karthi Chandra <chandra.karthi@gmail.com>
2016-09-14 13:58:21 -0400

    Add README file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Changes made to Working directory on September 13
+Happy Onam
 # Hello World
 
 ## September 12, 2016
---
+++ index.php
@@ -0,0 +1,17 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Home Page for Class Project</title>
+    </head>
+
+
+<body>
+  <br/>
+  <a href="index.php">Home</a>
+  <br/>
+  <?php echo "Hello World !"; ?><br />
+  <img src="images/guidePuppy1.png" width="250px" height="200px">
+</body>
+</html>
