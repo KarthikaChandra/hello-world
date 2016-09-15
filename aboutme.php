828c382931b228e5e461633f023894bf2f0f1dd8
Karthi Chandra <chandra.karthi@gmail.com>
2016-09-15 06:01:09 -0400

    added the aboutme page

--- aboutme.php
+++ aboutme.php
@@ -15,8 +15,9 @@
   <p><u>My Name: </u>Karthiyayini Chandra - Go by Karthi Chandra<br>
     <a href="https://www.linkedin.com/in/karthi-chandra-37a61b11?trk=nav_responsive_tab_profile" target="_blank">Linked In Profile</a>
   </p>
-
-  <p><u>About me:</u> - I have been doing web development for about 20 years when the internet took off.
+<img src="images/karthi@5am_pp.png">
+<br>
+  <p><u>About me:</u> - I have been doing web development for about 20 years and started in late 90's when the internet took off.
     <br>In my previous life was a Chemical Engineer but made the change to Web development during the DotCom boom. Have never
     regretted it.<br>
     I have been dabbling in a lot of Microsoft technologies inclding Classic ASP, Asp.net, SharePoint and SQL server.
@@ -25,17 +26,17 @@
      <u>Why I am I taking this course ?</u>
      <br>This is my third course @Harvard Online Extension school and hope to complete 2 more to get the Certificate in Web Technologies
      by end of 2017.<br />
-    These classes have given me tremendous amount of knowledge. Cannot thank the faculty enough.
+    These classes have given me tremendous amount of knowledge.
     <br>I hope to become a full stack developer and broaden my skillsets by taking this class. Though I am not using php for work I will definitely be using
     it to create content for non profit sites.
   </p>
 
   <p><u>Current Situation</u>
-    I had a bad accident @ the workplace on August 22 and had surgery and will be starting
-     a new position in a health care IT firm in Philly as SharePoint analyst. <br>
+    I am recovering from an accident related surgery and will be starting
+     a new position in a health care IT firm in Philly as SharePoint analyst soon.
      <i>Very excited about this.</i>
      <br />Was very fortunate to have excellent care at the hospital and at home.
-     The whole theme of my projects will be healing using conventional and new age methodologies.<br >
+     The whole theme of my projects will be healing applying conventional and non conventional methodologies.<br >
   </p>
   <h3>Listing of  projects</h3>
   <ol>
@@ -45,6 +46,11 @@
     <li>  <a href="#">P4 Project</a></li>
 
   </ol>
+  <h3>Portfolio</h3>
+  <ul>
+      <li><a href="http://medhas1.net/BG/default.html" target="_blank">JavasScript Programming Final Project Site</li>
+      <li><a href="https://medhas-public.sharepoint.com" target="_blank">Office 365 SharePoint Demo Site</li>
+  </ul>
   <?php
   echo "Last modified: " . date ("F d Y H:i:s.", getlastmod());
   ?>
