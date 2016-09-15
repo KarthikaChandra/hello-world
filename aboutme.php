74d2dab20a98b15972ae1cf9c84131adf79a63d5
Karthi Chandra <chandra.karthi@gmail.com>
2016-09-14 23:56:42 -0400

    made changes to title

--- aboutme.php
+++ aboutme.php
@@ -3,7 +3,7 @@
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8" />
-    <title>About Me Functions </title>
+    <title>About Me Project P1</title>
     <link rel="stylesheet" href="pure_css/pure.css">
     </head>
 
