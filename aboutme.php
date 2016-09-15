3958bb8f8d9779b21fd1d2eae0be91a4320106b8
Karthi Chandra <chandra.karthi@gmail.com>
2016-09-14 23:01:06 -0400

    changes made to server 9/15/2016

---
+++ aboutme.php
@@ -0,0 +1,16 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>About Me Functions </title>
+    <link rel="stylesheet" href="pure_css/pure.css">
+    </head>
+    <br/>
+    <a href="index.php">Home</a>
+    <br/>
+
+<body>
+
+</body>
+</html>
